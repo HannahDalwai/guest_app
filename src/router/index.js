6f431058e6e163dfbc60824fe298f850c7535ba9
HannahDalwai <hannahdalwai4@gmail.com>
2022-06-15 13:51:18 +0100

    my booking and  router

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import MyBooking from '../views/MyBooking.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
+    path: '/MyBooking',
+    name: 'MyBooking',
+    component: MyBooking
   }
 ]
 
